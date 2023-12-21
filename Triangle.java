85962b3a3ad4c60f45ad62e2af4e645e6745ec3c
HagarPeled <hagar.peled@post.runi.ac.il>
2023-12-21 13:26:20 +0200

    hello

--- Triangle.java
+++ Triangle.java
@@ -5,6 +5,11 @@
  */ 
 public class Triangle {
 	public static void main(String[] args) {
+		int a= Integer.parseInt(args[0]);
+		int b= Integer.parseInt(args[1]);
+		int c= Integer.parseInt(args[2]);
+		boolean result= ((a+b)>c); 
 		// Put your code here	
+		System.out.println(a + "," + b + "," + c + ":" + result);
 	}
 }
