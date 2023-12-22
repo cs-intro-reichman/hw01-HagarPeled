d45c0fd3c9b59c29d0357ddfe4368e50cff9cf6f
HagarPeled <hagar.peled@post.runi.ac.il>
2023-12-22 09:57:01 +0200

    homework 1

--- GenThree.java
+++ GenThree.java
@@ -6,10 +6,10 @@ public class GenThree {
 	public static void main(String[] args) {
 		int a= Integer.parseInt(args[0]);
 		int b= Integer.parseInt(args[1]);
-		double num1 = (double)(Math.random() * (b-a)+a);
-		double num2 = (double)(Math.random() * (b-a)+a);
-		double num3 = (double)(Math.random() * (b-a)+a);
-		double minnum = Math.min(num1,Math.min(num2,num3));
+		int num1 = (int)(Math.random() * (b-a+1)+a);
+		int num2 = (int)(Math.random() * (b-a+1)+a);
+		int num3 = (int)(Math.random() * (b-a+1)+a);
+		int minnum = Math.min(num1,Math.min(num2,num3));
 		System.out.println(num1);
 		System.out.println(num2);
 		System.out.println(num3);
